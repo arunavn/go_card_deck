ecba3bc6d67084d167263dba4a6b91a61218caa0
arunavn <arunavnarayan@gmail.com>
2023-08-10 21:24:09 +0530

    day4

--- deck.go
+++ deck.go
@@ -1,6 +1,12 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"os"
+	"strings"
+	"time"
+)
 
 // create a new type of deck, of type slice of str
 type deck []string
@@ -26,4 +32,38 @@ func (d deck) print() {
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
 
+}
+
+func (d deck) toString() string {
+	deckString := ""
+	// for _, x := range d {
+	// 	deckString = deckString + x + ","
+	// }
+	// return deckString[:len(deckString)-1]
+	deckString = strings.Join(d, ",")
+	return deckString
+}
+
+func (d deck) saveToFile(filePath string) {
+	byteDeck := []byte(d.toString())
+	os.WriteFile(filePath, byteDeck, 0666)
+}
+
+func newDeckFromFile(filename string) deck {
+	bs, err := os.ReadFile(filename)
+	if err != nil {
+		fmt.Println("Error", err)
+		os.Exit(1)
+	}
+	s := strings.Split(string(bs), ",")
+	return deck(s)
+}
+
+func (d deck) shuffleDeck() {
+	source := rand.NewSource(time.Now().UnixNano())
+	r := rand.New(source)
+	for i, _ := range d {
+		ri := r.Intn(len(d) - 1)
+		d[i], d[ri] = d[ri], d[i]
+	}
 }
--- main.go
+++ main.go
@@ -7,16 +7,22 @@ func main() {
 	// card := newCard()
 	// fmt.Println(card)
 	// cards = append(cards, "Six of Spades")
-	cards := newDeck()
+	// cards := newDeck()
+	cards := newDeckFromFile("test.txt")
+	// cards.saveToFile("test.txt")
+
 	// for _, x := range cards {
 	// 	fmt.Println(x)
 	// }
 	cards.print()
+	cards.shuffleDeck()
 	fmt.Println("Hand................................")
-	hand, remainingCards := deal(cards, 4)
-	hand.print()
-	fmt.Println("Remaining...........................")
-	remainingCards.print()
+	cards.print()
+	// fmt.Println("Hand................................")
+	// hand, remainingCards := deal(cards, 4)
+	// hand.print()
+	// fmt.Println("Remaining...........................")
+	// remainingCards.print()
 }
 
 func newCard() string {
